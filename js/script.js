e44f5e2fdf2a962bad4445d0144eb16d68e48fa4
Aleksey <example@gmail.com>
2020-12-09 15:44:04 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+ 
+let incr = 10,
+decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(4%2);
+console.log(3*3 === '9');
+
+const isChecked = false,
+isClose = false;
+
+console.log(isChecked || !isClose);
